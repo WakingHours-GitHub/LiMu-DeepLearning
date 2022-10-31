06f4f7d9cd876f1240312c3e5c4c434c7776497e
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-10-31 15:44:03 +0800

    10.31

--- Z_project/ORC_library_project/image_dataset.py
+++ Z_project/ORC_library_project/image_dataset.py
@@ -2,12 +2,15 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import os
 import cv2 as cv
- 
+from torchvision import transforms
+import numpy as np
 
-
-class OCR_Dataset(Dataset):
-    def __init__(self, train=True):
+class ImageDataset(Dataset):
+    def __init__(self, train=True, transforms=None, target_transfoms=None):
         self.root = "./image_data"
+        self.train = True
+        self.transforms = transforms
+        self.target_transfoms = target_transfoms
         if train == True:
             self.root = os.path.join(self.root, "train")
         else: 
@@ -19,18 +22,39 @@ class OCR_Dataset(Dataset):
         full_path = os.path.join(self.root, self.file_name_list[item])
         input = cv.imread(full_path, cv.IMREAD_GRAYSCALE)
         label = self.file_name_list[item]
+        if self.transforms != None:
+            input = self.transforms(input)
+        if self.target_transfoms != None:
+            label = self.target_transfoms(label)
+            label = label.reshape(label.shape[1:])
 
-        return input, label.split('.')[0]
+        return input, label
+
+
+
 
     def __len__(self):
         return len(self.file_name_list)
 
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
 
-
+def label2one_hot(label) -> np.ndarray:
+    label = np.array([int(ele) for ele in label.split(".")[0]])
+    label = convert_to_one_hot(label, 10)
+    return label
+    
 
 def main() -> None:
-    dataset_picture = OCR_Dataset()
-    print(dataset_picture[0][0].shape)
+    dataset_picture = ImageDataset(
+        transforms=transforms.ToTensor(), 
+        target_transfoms=transforms.Compose([
+            label2one_hot,
+            transforms.ToTensor()
+        ])
+    )
+    print(dataset_picture[0])
 
 
 
--- Z_project/ORC_library_project/main.py
+++ Z_project/ORC_library_project/main.py
@@ -1,6 +1,6 @@
 # from image_dataset import ImageDataset
 import os
-from re import I
+import numpy as np
 import cv2 as cv
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -23,10 +23,11 @@ device = torch.device("cuda:0")
 
 
 class ImageDataset(Dataset):
-    def __init__(self, train=True, transforms=None):
+    def __init__(self, train=True, transforms=None, target_transfoms=None):
         self.root = "./image_data"
         self.train = True
         self.transforms = transforms
+        self.target_transfoms = target_transfoms
         if train == True:
             self.root = os.path.join(self.root, "train")
         else: 
@@ -38,13 +39,26 @@ class ImageDataset(Dataset):
         full_path = os.path.join(self.root, self.file_name_list[item])
         input = cv.imread(full_path, cv.IMREAD_GRAYSCALE)
         label = self.file_name_list[item]
-        if transforms != None:
+        if self.transforms != None:
             input = self.transforms(input)
+        if self.target_transfoms != None:
+            label = self.target_transfoms(label)
+            label = label.reshape(label.shape[1:])
 
-        return input, label.split('.')[0]
+        return input, label
 
     def __len__(self):
         return len(self.file_name_list)
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
+
+def label2one_hot(label) -> np.ndarray:
+    label = np.array([int(ele) for ele in label.split(".")[0]])
+    label = convert_to_one_hot(label, 10)
+    return label
+
 
 
 class OCR_netword(nn.Module):
@@ -73,7 +87,11 @@ def loss_function(y_hat, y_true):
 
 def train() -> None:
     image_dataset = ImageDataset(
-        transforms=transforms.ToTensor()
+        transforms=transforms.ToTensor(), 
+        target_transfoms=transforms.Compose([
+            label2one_hot,
+            transforms.ToTensor()
+        ])
     )
     image_dataloader = DataLoader(
         image_dataset,
@@ -90,10 +108,11 @@ def train() -> None:
 
             images = images.cuda(0)
             print(images.shape)
-            # labels = labels.cuda()
+            labels = labels.cuda()
             net = net.to(device)
             y_hat = net(images)
             print(y_hat.shape)
+            print(labels.shape)
             # loss_function(y_hat, labels)
 
 
@@ -114,6 +133,7 @@ def train() -> None:
 
 if __name__ == "__main__":
     train()
+    pass
 
 
 
